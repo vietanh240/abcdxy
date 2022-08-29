49f067fa5df82202b2c3ee71b634a06ce9f413aa
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2022-08-29 21:22:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Thu Hương';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100075505382102';
-const eventName = 'Hương Thu';
+const eventName = 'Happy Birthday';
 const titleCard = 'Chúc Mừng Sinh Nhật Trần Thu Hương';
 const contentCard = 'Chúc abc tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau ';
 
