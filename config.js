30cd2694d8ad5ff265e43bb8396699118f1e37cf
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2022-08-29 21:08:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Linh Phương';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Tặng người ấy';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100075505382102';
+const eventName = 'Hương Thu';
+const titleCard = 'Chúc Mừng Sinh Nhật Trần Thu Hương';
 const contentCard = 'Chúc abc tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
